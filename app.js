99bbf0c1a042e254eacd51ba69317841f8bc418f
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-25 15:28:04 +0100

    done zipper

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const Node = require("./Node");
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
 const a = new Node("A");
 const b = new Node("B");
@@ -180,3 +185,38 @@ const reverseListRec = (head, prev = null) => {
 
 console.log("\nReverse list rec");
 printList(reverseListRec(d));
+
+const q = new Node("Q");
+const r = new Node("R");
+
+q.next = r;
+console.log("\nHead2:", q);
+
+// zipper lists
+const zipperList = (head1, head2) => {
+  let tail = head1;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
+
+  while (current1 && current2) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count++;
+  }
+
+  if (current1) {
+    tail.next = current1;
+  }
+  if (current2) {
+    tail.next = current2;
+  }
+
+  return head1;
+};
